bac32e04908044bf72f424c46297758c28b4f375
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-10-10 11:55:23 +0200

    created and exported second module and with multiple statements

---
+++ module_2.js
@@ -0,0 +1,11 @@
+// exporting two statements or more from a module
+const sum = (num1, num2) => num1 + num2;
+const PI = 3.14;
+class Employee {
+    constructor(){
+        console.log('object created');
+    }
+}
+
+// exporting statements as an object
+module.exports = {sum: sum, PI: PI, Employee: Employee};
