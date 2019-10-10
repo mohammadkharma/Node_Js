3fda0139ab2c7e7f3461b92e3d7bf41b9b6c8544
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-10-10 11:56:13 +0200

    imported and used second module with multiple statements

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-// importing modules by requiring the file name
+// importing a modules by requiring it's name (file name)
 const module_1 = require ('./module_1');
+const module_2 = require ('./module_2');
+
+// ------------------------------------------------------------------------------------------------------- //
+
+// module_1 (single statement)
 console.log(module_1); // [Function: name]
 
-// using a function from a module and passing parameters to it
+// using a module with a single function and passing parameters to it
 console.log(module_1('Micheal', ' Jackson')); // Micheal Jackson
+
+// ------------------------------------------------------------------------------------------------------- //
+
+// module_2 (two statements and more)
+console.log(module_2); // {sum: [Function: sum], PI: 3.14, Employee: [Function: Employee]}
+
+// using the module's statements
+console.log(module_2.sum(1, 2)); // 3
+console.log(module_2.PI); // 3.14
+console.log(new module_2.Employee()); // Employee {}
+
+// ------------------------------------------------------------------------------------------------------- //
+
+
