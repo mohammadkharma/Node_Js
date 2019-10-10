e8eec55d842cd7547c30e14eb90b9f0a876e11cd
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-10-10 13:45:31 +0200

    added EventEmitter section

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const module_2 = require ('./module_2');
 
 // ------------------------------------------------------------------------------------------------------- //
 
-// module_1 (single statement)
+// module_1: (single statement)
 console.log(module_1); // [Function: name]
 
 // using a module with a single function and passing parameters to it
@@ -12,7 +12,7 @@ console.log(module_1('Micheal', ' Jackson')); // Micheal Jackson
 
 // ------------------------------------------------------------------------------------------------------- //
 
-// module_2 (two statements and more)
+// module_2: (two statements and more)
 console.log(module_2); // {sum: [Function: sum], PI: 3.14, Employee: [Function: Employee]}
 
 // using the module's statements
@@ -22,4 +22,41 @@ console.log(new module_2.Employee()); // Employee {}
 
 // ------------------------------------------------------------------------------------------------------- //
 
+// EventEmitter:
+// EventEmitter is a class from the event built in  module
+const EventEmitter = require('events');
 
+// creating an instance of EventEmitter class
+const myEventEmitter = new EventEmitter();
+
+// attaching methods to the EventEmitter
+// the method 'on' takes 2 arguments: name of the event, function to be executed when the event occurs
+myEventEmitter.on('add', (num1, num2) => {
+    console.log('add even has occurred');
+    console.log(num1 + num2);
+});
+
+// emitting the event by passing the event name as first argument, and the value of the event function
+myEventEmitter.emit('add', 1, 2); // myEvent has occurred, 3
+
+// creating classes by inheriting from the EventEmitter
+class Person extends EventEmitter {
+    constructor(name){
+        super(),
+        this._name = name;
+    }
+
+    get name() {
+        return this._name;
+    }
+}
+
+// creating instance of the Person class, then attaching 'on' method and emitting it
+let micheal = new Person('Micheal');
+micheal.on('name', ()=>{
+    console.log('my name is ' + micheal.name)
+});
+
+micheal.emit('name'); // my name is Micheal
+
+// ------------------------------------------------------------------------------------------------------- //
