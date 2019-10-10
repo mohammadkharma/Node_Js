adbab04937592a5fd74695835a608292eb25c8ab
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-10-10 11:30:12 +0200

    imported the first module and used it

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('hello world');
+// importing modules by requiring the file name
+const module_1 = require ('./module_1');
+console.log(module_1); // [Function: name]
+
+// using a function from a module and passing parameters to it
+console.log(module_1('Micheal', ' Jackson')); // Micheal Jackson
